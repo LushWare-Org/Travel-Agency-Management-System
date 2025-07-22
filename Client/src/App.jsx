53d46f1ea9f5a5316ca778059c9af7cbc36150a7
Anuradha Herath <anuradhaherath2001@gmail.com>
2025-07-22 17:38:41 +0530

    Add LandingHeader component to App for improved navigation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import axios from 'axios';
+import LandingHeader from './Landing/LandingHeader';
 
 // AUTHENTICATION DISABLED - ProtectedRoute import commented out
 // import ProtectedRoute from './Components/ProtectedRoute';
@@ -30,43 +31,46 @@ axios.defaults.withCredentials = true;
 export default function App() {
   return (
     <Suspense fallback={<div className="text-center py-10">Loading…</div>}>
-      <Routes>
-        {/* public */}
-        <Route path="/"       element={<><Home/><Footer/></>} />
-        <Route path="/login"  element={<><Login/><Footer/></>} />
-        <Route path="/register" element={<><Register/><Footer/></>} />
+      <LandingHeader />
+      <div style={{ paddingTop: '80px' }}>
+        <Routes>
+          {/* public */}
+          <Route path="/"       element={<><Home/><Footer/></>} />
+          <Route path="/login"  element={<><Login/><Footer/></>} />
+          <Route path="/register" element={<><Register/><Footer/></>} />
 
-        {/* sidebar pages (public) */}
-        <Route path="/contact"        element={<SidebarLayout><ContactForm/></SidebarLayout>} />
-        <Route path="/search"         element={<SidebarLayout><Search/></SidebarLayout>} />
-        <Route path="/special-offers" element={<SidebarLayout><SpecialOffers/></SidebarLayout>} />
+          {/* sidebar pages (public) */}
+          <Route path="/contact"        element={<SidebarLayout><ContactForm/></SidebarLayout>} />
+          <Route path="/search"         element={<SidebarLayout><Search/></SidebarLayout>} />
+          <Route path="/special-offers" element={<SidebarLayout><SpecialOffers/></SidebarLayout>} />
 
-        {/* protected - AUTHENTICATION DISABLED, all routes now accessible */}
-        <Route path="/settings" element={<SidebarLayout><Settings/></SidebarLayout>} />
-        <Route path="/bookings" element={<SidebarLayout><Bookings/></SidebarLayout>} />
-        <Route path="/bookingRequest" element={<SidebarLayout><BookingRequest/></SidebarLayout>} />
+          {/* protected - AUTHENTICATION DISABLED, all routes now accessible */}
+          <Route path="/settings" element={<SidebarLayout><Settings/></SidebarLayout>} />
+          <Route path="/bookings" element={<SidebarLayout><Bookings/></SidebarLayout>} />
+          <Route path="/bookingRequest" element={<SidebarLayout><BookingRequest/></SidebarLayout>} />
 
-        {/* hotel search */}
+          {/* hotel search */}
 
-        {/* hotel/room profiles */}
-        <Route path="/hotels/:hotelId" element={<SidebarLayout><HotelProfile/></SidebarLayout>} />
-        <Route path="/hotels/:hotelId/rooms/:roomId" element={<SidebarLayout><RoomProfile/></SidebarLayout>} />
+          {/* hotel/room profiles */}
+          <Route path="/hotels/:hotelId" element={<SidebarLayout><HotelProfile/></SidebarLayout>} />
+          <Route path="/hotels/:hotelId/rooms/:roomId" element={<SidebarLayout><RoomProfile/></SidebarLayout>} />
 
-        {/* tour search */}
-        <Route path="/tours" element={<SidebarLayout><Tours/></SidebarLayout>} />
+          {/* tour search */}
+          <Route path="/tours" element={<SidebarLayout><Tours/></SidebarLayout>} />
 
-        {/* tour packages */}
-        <Route path="/tours/:tourId" element={<SidebarLayout><TourDetails/></SidebarLayout>} />
+          {/* tour packages */}
+          <Route path="/tours/:tourId" element={<SidebarLayout><TourDetails/></SidebarLayout>} />
 
-        {/* user profile - AUTHENTICATION DISABLED */}
-        <Route path="/profile" element={<SidebarLayout><UserProfile/></SidebarLayout>} />
+          {/* user profile - AUTHENTICATION DISABLED */}
+          <Route path="/profile" element={<SidebarLayout><UserProfile/></SidebarLayout>} />
 
-        {/* admin only - AUTHENTICATION DISABLED, now accessible to all */}
-        <Route path="/admin" element={<AdminPanel/>} />
+          {/* admin only - AUTHENTICATION DISABLED, now accessible to all */}
+          <Route path="/admin" element={<AdminPanel/>} />
 
-        {/* catch‑all */}
-        <Route path="*" element={<Home/>} />
-      </Routes>
+          {/* catch‑all */}
+          <Route path="*" element={<Home/>} />
+        </Routes>
+      </div>
     </Suspense>
   );
 }
